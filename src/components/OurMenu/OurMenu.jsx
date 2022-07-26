7b03679a5dc09e5f709d88585a4a1e3e91428bcf
Kriszti <kecskeskriszti22@gmail.com>
2022-07-26 15:49:14 +0200

    menu listed out

--- src/components/OurMenu/OurMenu.jsx
+++ src/components/OurMenu/OurMenu.jsx
@@ -1,6 +1,11 @@
+import { foodMenuList } from '../../constants/foodMenuList';
+import AddIcon from '@mui/icons-material/Add';
+import RemoveIcon from '@mui/icons-material/Remove';
+import ShoppingCartCheckoutIcon from '@mui/icons-material/ShoppingCartCheckout';
 import './ourMenu.css';
 
 export default function OurMenu() {
+
     return (
         <section id="menu">
             <div className='menuTitle'>
@@ -8,7 +13,25 @@ export default function OurMenu() {
                 <h2>The Most Popular</h2>
             </div>
             <div className='filteringOptions'></div>
-            <div className='foodContainer'></div>
+            <div className='foodContainer'>
+                {foodMenuList.map(({ id, image, name, price }) => (
+                    <div className='foodCard' key={id}>
+                        <img src={image} alt={image} />
+                        <h4>{name}</h4>
+                        <h5>{price}</h5>
+                        <div className='shopping'>
+                            <div className='shopCount'>
+                                <button><RemoveIcon sx={{ fontSize: 25 }} /></button>
+                                <p>0</p>
+                                <button><AddIcon sx={{ fontSize: 25 }} /></button>
+                            </div>
+                            <button className='toCart'>
+                                <ShoppingCartCheckoutIcon sx={{ fontSize: 19, backgroundColor: "#eeb528" }} />
+                            </button>
+                        </div>
+                    </div>
+                ))}
+            </div>
             <div className='showMoreContainer'>
                 <button>Show More</button>
             </div>
---
+++ src/constants/foodMenuList.js
@@ -0,0 +1,132 @@
+import pizza1 from "../assets/images/food/pizza1.png";
+import pizza2 from "../assets/images/food/pizza2.png";
+import pizza3 from "../assets/images/food/pizza3.png";
+import pizza4 from "../assets/images/food/pizza4.png";
+import pizza5 from "../assets/images/food/pizza5.png";
+import pizza6 from "../assets/images/food/pizza6.png";
+
+import hamburger1 from "../assets/images/food/hamburger1.png";
+import hamburger2 from "../assets/images/food/hamburger2.png";
+import hamburger3 from "../assets/images/food/hamburger3.png";
+import hamburger4 from "../assets/images/food/hamburger4.png";
+import hamburger5 from "../assets/images/food/hamburger5.png";
+
+import dessert1 from "../assets/images/food/dessert1.png";
+import dessert2 from "../assets/images/food/dessert2.png";
+import dessert3 from "../assets/images/food/dessert3.png";
+import dessert4 from "../assets/images/food/dessert4.png";
+
+import drink1 from "../assets/images/food/drink1.png";
+import drink2 from "../assets/images/food/drink2.png";
+import drink3 from "../assets/images/food/drink3.png";
+
+export const foodMenuList = [
+  {
+    id: 1,
+    image: pizza1,
+    name: "Green Salmon",
+    price: "$2.50",
+  },
+  {
+    id: 2,
+    image: hamburger1,
+    name: "Double Cheese Burger",
+    price: "$2.30",
+  },
+  {
+    id: 3,
+    image: dessert1,
+    name: "Cheese Cake",
+    price: "$2.00",
+  },
+  {
+    id: 4,
+    image: drink1,
+    name: "Red Berry Lemonade",
+    price: "$1.30",
+  },
+  {
+    id: 5,
+    image: pizza2,
+    name: "Sweetened Salmon",
+    price: "$2.50",
+  },
+  {
+    id: 6,
+    image: hamburger2,
+    name: "Long Fish Burger",
+    price: "$2.30",
+  },
+  {
+    id: 7,
+    image: dessert2,
+    name: "Red Velvet Cake",
+    price: "$2.00",
+  },
+  {
+    id: 8,
+    image: drink2,
+    name: "Mojito Lemonade",
+    price: "$1.30",
+  },
+  {
+    id: 9,
+    image: pizza3,
+    name: "Salami Pizza",
+    price: "$2.50",
+  },
+  {
+    id: 10,
+    image: hamburger3,
+    name: "Cheesy Long Burger",
+    price: "$2.30",
+  },
+  {
+    id: 11,
+    image: dessert3,
+    name: "Tiramisu",
+    price: "$2.00",
+  },
+  {
+    id: 12,
+    image: drink3,
+    name: "Iced Coca Cola",
+    price: "$1.30",
+  },
+  {
+    id: 13,
+    image: pizza4,
+    name: "Olive Pizza",
+    price: "$2.50",
+  },
+  {
+    id: 14,
+    image: hamburger4,
+    name: "Extra Meety Burger",
+    price: "$2.30",
+  },
+  {
+    id: 15,
+    image: dessert4,
+    name: "Macaron Cake",
+    price: "$2.00",
+  },
+  {
+    id: 16,
+    image: pizza5,
+    name: "Shrimp Pizza",
+    price: "$2.50",
+  },
+  {
+    id: 17,
+    image: hamburger5,
+    name: "Cheese Burger",
+    price: "$2.30",
+  },
+  {
+    id: 18,
+    image: pizza6,
+    name: "Chicken Salad Pizza",
+    price: "$2.50",
+  },
+];
